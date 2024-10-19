54c38a73262c9d1e48303b0c9e520e3e0b442bf0
Andres <andres.03.12.2003@gmail.com>
2024-10-19 11:48:38 -0400

    add origin-true

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     // Habilitar CORS
   app.enableCors({
-    origin: [
-      'http://localhost:5174',
-      'http://localhost:5173',
-      'https://vercel.live/link/frontend-web-graphics.vercel.app?via=project-dashboard-alias-list&p=1'
-    ], // Puedes especificar tu origen aquí o usar '*' para todos
+    origin: true,// Puedes especificar tu origen aquí o usar '*' para todos
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Si necesitas enviar cookies o autenticación
   });
