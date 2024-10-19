354f800f3a5324f4dfc9699c934c37ad9eb3bcd4
Andres <andres.03.12.2003@gmail.com>
2024-10-19 12:40:03 -0400

    put-origin

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     // Habilitar CORS
   app.enableCors({
-    origin: '*',// Puedes especificar tu origen aquí o usar '*' para todos
+    origin: [
+      'http://localhost:5173',
+      'https://frontend-web-graphics.vercel.app',
+      'https://vercel.live/link/frontend-web-graphics.vercel.app?via=project-dashboard-alias-list&p=1'
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: false, // Si necesitas enviar cookies o autenticación
+    credentials: true, // Si necesitas enviar cookies o autenticación
   });
   app.setGlobalPrefix('api');  // Añade un prefijo global para las rutas
   await app.listen(3000);
